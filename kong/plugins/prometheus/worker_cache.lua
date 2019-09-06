b383140e3f45e8c3755a1321e36c57fa6b98f8d1
Guanlan Dai <hackertian@gmail.com>
2019-09-06 18:40:12 -0400

    perf(*) add worker_cache WIP

---
+++ kong/plugins/prometheus/worker_cache.lua
@@ -0,0 +1,9 @@
+-- TODO cache layer per work, sync up with shdict periodically
+--
+function per_worker_inc(name, value, label_values)
+-- TODO 
+end
+
+function per_worker_observe(name, value, label_values)
+-- TODO
+end
